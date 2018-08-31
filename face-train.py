2cc049579f1ecacaef69f868c187d99f8e8ef349
Matias Lin <42155192+MelinUCSD@users.noreply.github.com>
2018-08-31 11:10:39 -0700

    Delete face-train.py

--- face-train.py
+++
@@ -1,2 +0,0 @@
-# This file is where we will train the program to identify 48 2018 SPIS students, 12 mentors and 3 instructors.
-# We will need to create a file and save it, so that we will be able to use it in "face-recognition.py".
