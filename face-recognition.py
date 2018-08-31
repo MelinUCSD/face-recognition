b943d8db3a83354dfa6573e256ee480a7fcd0cb6
Matias Lin <42155192+MelinUCSD@users.noreply.github.com>
2018-08-31 10:00:50 -0700

    Update face-recognition.py

--- face-recognition.py
+++ face-recognition.py
@@ -1,4 +1,3 @@
-
 ##########################################################################################################
 ##                                                                                                      ##
 ##            Welcome to (*INSERT GOOD NAME HERE*)! Created by Dare Hunt and Matias Lin                 ##
@@ -29,20 +28,27 @@ while(True):
 
     #Converting the frames from BGR to Gray Scale
     gray_scale = cv2.cvtColor(scope, cv2.COLOR_BGR2GRAY)
-
-    cv2.imshow('scope', scope)
     
     #Face Identifier
-    faces_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    faceId = faces_cascade.detectMultiScale(gray_scale, 1.5, 5)
+    faces_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_default.xml')
+    faceId = faces_cascade.detectMultiScale(gray_scale, scaleFactor=1.5, minNeighbors = 5)
 
     #Square and Label around the face
     for (x, y, h, w) in faceId:
-        cv2.rectangle(scope, (x,y), (x+w,y+h), (0,238,238), 3)
-        #soi = scope of interest
-        soi_gray = gray_scale[y:y+h, x:x+w]
-        soi_scope = scope[y:y+h, x:x+w]    
+        #print(x,y,h,w) #Checking if haarcascade is working
 
+        #Set up for Rectangle
+        color = (255, 255, 0) #BGR
+        stroke = 2
+        endx = x + w
+        endy = y + h
+        cv2.rectangle(scope, (x,y), (endx, endy), color, stroke)
+
+        #roi = region of interest
+        soi_gray = gray_scale[y:y+h, x:x+w]
+        soi_scope = scope[y:y+h, x:x+w]
+
+    cv2.imshow('scope', scope)
     #Escape Key
     if cv2.waitKey(1) & 0xFF == ord('`'):
       break
