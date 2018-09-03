3d72adf514def54532f65bc9b1adc50ece223f75
unknown <melin@ucsd.edu>
2018-09-03 11:27:01 -0700

    Project Work: 9/03/2018

--- src/face-recognition.py
+++ src/face-recognition.py
@@ -16,8 +16,34 @@
 
 import cv2
 import numpy as np
+import pickle
+from PIL import Image
+import os
+import shutil
+import os.path
+from os import path
 
 webCam = cv2.VideoCapture(0)
+faces_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_default.xml')
+
+#Creating recognizer and reading data
+recognition = cv2.face.LBPHFaceRecognizer_create()
+recognition.read("training/trained.yml")
+
+#Inverting the key-value pair in user
+def dictinvers(user):
+    userInfo = {}
+    for k, v in user.items():
+        keys = userInfo.setdefault(v, [])
+        keys.append(k)
+    return userInfo
+
+#Getting the labels
+user = {}
+pickle_in = open("training/ids.pickle", "rb")
+user = pickle.load(pickle_in)
+userInfo = dictinvers(user)
+#print(dictinvers(user))
 
 while(True):
     #Activate webCam
@@ -30,29 +56,66 @@ while(True):
     gray_scale = cv2.cvtColor(scope, cv2.COLOR_BGR2GRAY)
     
     #Face Identifier
-    faces_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_default.xml')
-    faceId = faces_cascade.detectMultiScale(gray_scale, scaleFactor=1.5, minNeighbors = 5)
+    faceId = faces_cascade.detectMultiScale(gray_scale, scaleFactor=1.5, minNeighbors = 6)
 
     #Square and Label around the face
-    for (x, y, h, w) in faceId:
+    for (x, y, w, h) in faceId:
         #print(x,y,h,w) #Checking if haarcascade is working
+
+        save_image = "last_user.jpg"
+        cv2.imwrite(save_image,scope)
 
         #Set up for Rectangle
         color = (255, 255, 0) #BGR
-        stroke = 2
+        thickness = 2
         endx = x + w
         endy = y + h
-        cv2.rectangle(scope, (x,y), (endx, endy), color, stroke)
+        cv2.rectangle(scope, (x,y), (endx, endy), color, thickness)
 
         #roi = region of interest
-        soi_gray = gray_scale[y:y+h, x:x+w]
-        soi_scope = scope[y:y+h, x:x+w]
+        roi_gray = gray_scale[y:y+h, x:x+w]
+        #roi_scope = scope[y:y+h, x:x+w]
+
+        #Setting up the recognizer 
+        myId, conf = recognition.predict(roi_gray)
+        if conf >=4 and conf <= 85:
+            #print(myId)
+            myName = str(userInfo[myId])
+            print(myName)
+            cv2.putText(scope, myName, (x,y), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255,255,255), 3)
+            display = "last_user_display.jpg"
+            cv2.imwrite(display,scope)
 
     cv2.imshow('scope', scope)
     #Escape Key
-    if cv2.waitKey(1) & 0xFF == ord('`'):
-      break
+    if cv2.waitKey(20) & 0xFF == ord('`'):
+        last_user = Image.open("last_user_display.jpg")
+        last_user.show()
+        print("\n\n\n\n#########################################################################################################\n\n")
+        print("Thank you for using ... ! Please take a moment to complete the following survey.\n\n")
+        right_wrong = input("Was the prediction correct? ").lower()
+        if right_wrong == ("yes") or right_wrong == ('y'):
+            print("\n\n\n\n\n\nGreat! Please come back!\n\n")
+            print("\n\n\n\n#########################################################################################################\n\n")
+            break
+        elif right_wrong == ('no') or right_wrong  == ("n"):
+            print("I'm sorry to hear that. It will be of great help if you answer the next question!")
+            name = input("What's your name? ")
+            destination = ("images/%s" %name)
+            #newImage = os.rename(save_image, name + ".jpg") #FIND HOW TO RENAME BUT MAKE IT A JPG
+            #print(path.isdir(destination))
+            if path.isdir(destination) == True:
+                shutil.move(save_image,destination)
+            else:
+                os.mkdir("images/%s"%name)
+                shutil.move(save_image, destination)
+            print("\n\n\n\nThank you very much!\n\n")
+            print("\n\n\n\n#########################################################################################################\n\n")
+            break
+        else:
+            print("No comprendo")
+            break
 
-#This will free the Camera for future uses
+#This will free the Camera for future2 uses
 webCam.release()
 cv2.destroyAllWindows()
--- src/face-train.py
+++ src/face-train.py
@@ -1,15 +1,26 @@
 # This file is where we will train the program to identify 48 2018 SPIS students, 12 mentors and 3 instructors.
 # We will need to create a file and save it, so that we will be able to use it in "face-recognition.py".
-
 import os
+from PIL import Image
+import cv2
+import numpy as np
+import pickle
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+#Main/Root directory
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) #__file__ current file
 data_dir = os.path.join(PROJECT_ROOT, "images")
 #print(data_dir)
 
+#Face detector for training images
+faces_cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_default.xml')
 
-images = []
+#Data Station setup
+userNumber = 0  #Assigning numbers instead of names
+userInfo =  {}
+labels = []
+train_photos = []
 
+#Looping through our src\images folder
 for dirName, subdirList, fileList in os.walk(data_dir):
     #print(fileList)
     #print(dirName)
@@ -17,6 +28,44 @@ for dirName, subdirList, fileList in os.walk(data_dir):
     for file in fileList:
         #print(file)
         if file.endswith(".png") or file.endswith(".jpg"):
-            if  
-            images.append(file)
-print(images)
+            path = os.path.join(dirName,file)
+            names = os.path.basename(dirName)   #Getting the names of the users
+            #print(path)
+            #print(names)
+            if names not in userInfo:
+                userInfo[names] = userNumber   
+                userNumber += 1
+            myId = userInfo[names] #Recognizing people by their user number
+            #print(myId)
+
+            filt_photos = Image.open(path).convert('L') #Converts the data images to grayscale
+            #filt_photos.show()
+            npArray = np.array(filt_photos, "uint8") #Taking every pixel value and turning it into a numpy array
+            #print(npArray) #Now we can see the pictures in numbers
+            #Apply cascade, now that the images are in grayscale
+            faceId = faces_cascade.detectMultiScale(npArray) #, scaleFactor=1.3, minNeighbors = 3
+            #print(faceId)
+            
+            for (x,y,w,h) in faceId:
+                roi_grayscale = npArray[y:y+h, x:x+w]
+                #print(roi_grayscale)
+                train_photos.append(roi_grayscale)
+                labels.append(myId)
+
+#print(userInfo)
+#print(labels)
+#print(train_photos)
+
+
+#Using pickle to convert a python object into string.
+pickle_out = open("training/ids.pickle","wb")
+pickle.dump(userInfo,pickle_out)
+pickle_out.close()
+
+#Train
+#Creating Recognizer
+recognition = cv2.face.LBPHFaceRecognizer_create()
+'''We are working with LBPH, because we won't need to worry about the brightness. It works with the idividual pixels '''
+#print(len(train_photos), len(np.array(labels)))
+recognition.train(train_photos, np.array(labels))
+recognition.save("training/trained.yml")
